2b15d14dff24a997bc7147609bcf4745c2893677
Ed Vidal Jr <develone@sbcglobal.net>
2024-07-25 11:15:49 -0600

    testing using sdk at repo instead of ~/sdk/pico-sdk
    git clone https://github.com/develone/pico-examples.git -b dev
    
    git clone https://github.com/develone/pico-examples.git -b test-dev
    cd pico-examples/
    
    ./fetch-sdk.sh
    
    need to unset PICO_SDK_PATH
    
    . ./set-sdk.sh
    
    mkdir build
    
    cd build
    
    cmake ..
    
    -- Configuring done
    CMake Error at pico-sdk/lib/tinyusb/examples/host/msc_file_explorer/CMakeLists.txt:13 (add_executable):
      Cannot find source file:
    
        /lib/fatfs/source/ff.c
    
      Tried extensions .c .C .c++ .cc .cpp .cxx .cu .mpp .m .M .mm .ixx .cppm .h
      .hh .h++ .hm .hpp .hxx .in .txx .f .F .for .f77 .f90 .f95 .f03 .hip .ispc
    
    -- Generating done
    CMake Generate step failed.  Build files cannot be regenerated correctly.
    
    [ 93%] Building C object usb/device/dev_hid_composite/CMakeFiles/dev_hid_composite.dir/main.c.obj
    /home/devel/pico-examples/usb/device/dev_hid_composite/main.c:226:6: error: conflicting types for 'tud_hid_report_complete_cb'; have 'void(uint8_t,  const uint8_t *, uint8_t)' {aka 'void(unsigned char,  const unsigned char *, unsigned char)'}
      226 | void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint8_t len)
          |      ^~~~~~~~~~~~~~~~~~~~~~~~~~
    
    [100%] Building C object watchdog/hello_watchdog/CMakeFiles/hello_watchdog.dir/__/__/pico-sdk/src/rp2_common/pico_stdio_uart/stdio_uart.c.obj
    [100%] Linking CXX executable hello_watchdog.elf
    [100%] Built target hello_watchdog

---
+++ fetch-sdk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+git clone https://github.com/develone/pico-sdk.git
+cd pico-sdk
+git submodule update --init
---
+++ set-sdk.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PICO_SDK_PATH=/home/devel/pico-examples/pico-sdk
+env | grep PICO_SDK
