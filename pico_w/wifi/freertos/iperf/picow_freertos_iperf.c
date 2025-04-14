9b0998e4e039a69511f52df0dee249de2e6f1fd5
Edward Vidal Jr <develone@sbcglobal.net>
2025-04-14 11:09:03 -0600

    adding tasks

--- pico_w/wifi/freertos/iperf/picow_freertos_iperf.c
+++ pico_w/wifi/freertos/iperf/picow_freertos_iperf.c
@@ -7,20 +7,82 @@
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 
+#include <string.h>
+#include "lwip/dns.h"
+#include "lwip/pbuf.h"
+#include "lwip/udp.h"
+
 #include "lwip/netif.h"
 #include "lwip/ip4_addr.h"
 #include "lwip/apps/lwiperf.h"
 
+#include "hardware/gpio.h"
+//#include "hardware/adc.h"
+#include "tcp_debug.h"
+#include "head_tail.h"
+#include "mqtt_extra.h"
+u8_t lp;
+u8_t alarm_hour;
+u8_t alarm_min;
+u8_t alarm_sec;
+u8_t remote_index;
+u8_t cmd;
+char houralarm[3];
+char minalarm[3];
+char secalarm[3];
+char tmp[80];
+
+char * ptrhead;
+char * ptrtail;
+char * ptrendofbuf;
+char * ptrtopofbuf;
+char client_message[BUF_SIZE]; 
+//mqtt_request_cb_t pub_mqtt_request_cb_t; 
+
+u16_t mqtt_port = 1883;
+
+char PUB_PAYLOAD_T[]="                                       ";
+char PUB_PAYLOAD_SCR_T[]="                                       ";
+char PUB_EXTRA_ARG_T[] = "test";
+u16_t payload_t_size;
+
+
+
+/* Choose 'C' for Celsius or 'F' for Fahrenheit. */
+//#define TEMPERATURE_UNITS 'F'
+char TEMPERATURE_UNITS;
+char  unit;
+float retflg;
+uint32_t result;
+const float conversion_factor = 3.3f / (1 << 12);
+float battery;
+
+char * ptrhead;
+char * ptrtail;
+char * ptrendofbuf;
+char * ptrtopofbuf;
+char client_message[BUF_SIZE];
 #include "FreeRTOS.h"
 #include "task.h"
 
 #ifndef RUN_FREERTOS_ON_CORE
 #define RUN_FREERTOS_ON_CORE 0
 #endif
-
+#define BATT_TASK_PRIORITY
+#define SOCKET_TASK_PRIORITY			( tskIDLE_PRIORITY + 6UL )
+#define ADC_TASK_PRIORITY				( tskIDLE_PRIORITY + 8UL )
+#define NTP_TASK_PRIORITY				( tskIDLE_PRIORITY + 5UL )
 #define TEST_TASK_PRIORITY				( tskIDLE_PRIORITY + 2UL )
 #define BLINK_TASK_PRIORITY				( tskIDLE_PRIORITY + 1UL )
 
+#include "lwip/apps/mqtt.h"
+#include "mqtt_example.h"
+#include "pico/util/datetime.h"
+static volatile bool fired = false;
+
+u8_t alarm_flg=0;
+u8_t open_flg=0;
+u8_t close_flg=0;
 #if CLIENT_TEST && !defined(IPERF_SERVER_IP)
 #error IPERF_SERVER_IP not defined
 #endif
@@ -37,6 +99,137 @@ static void iperf_report(void *arg, enum lwiperf_report_type report_type,
 
     printf("Completed iperf transfer of %d MBytes @ %.1f Mbits/sec\n", mbytes, mbits);
     printf("Total iperf megabytes since start %d Mbytes\n", total_iperf_megabytes);
+}
+
+/* References for this implementation:
+ * raspberry-pi-pico-c-sdk.pdf, Section '4.1.1. hardware_adc'
+ * pico-examples/adc/adc_console/adc_console.c */
+float read_onboard_temperature(char unit) {
+    
+    /* 12-bit conversion, assume max value == ADC_VREF == 3.3 V */
+    float conversionFactor = 3.3f / (1 << 12);
+
+    float adc = (float)adc_read() * conversionFactor;
+    float tempC = 27.0f - (adc - 0.706f) / 0.001721f;
+
+    if (unit == 'C') {
+        return tempC;
+    } else if (unit == 'F') {
+        return tempC * 9 / 5 + 32;
+    }
+
+    return -50.0f;
+}
+
+static void alarm_callback(void) {
+    datetime_t t = {0};
+    rtc_get_datetime(&t);
+    char datetime_buf[256];
+    char *datetime_str = &datetime_buf[0];
+    datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
+    printf("Alarm Fired At %s\n", datetime_str);
+    sprintf(tmp,"Alarm Fired %s ",datetime_str);
+    ptrhead = head_tail_helper(ptrhead, ptrtail, ptrendofbuf, ptrtopofbuf, tmp);
+    //printf("client_message %s\n",client_message);
+    stdio_flush();
+    fired = true;
+    alarm_flg=1;
+}
+ 
+
+void adc_task(__unused void *params) {
+    //bool on = false;
+    adc_init();
+ 
+    
+    while (true) {
+	adc_set_temp_sensor_enabled(true);
+    	adc_select_input(4); 
+	float temperature = read_onboard_temperature(TEMPERATURE_UNITS);
+     
+    
+        if (temperature != -50.0) {
+            //printf("Onboard temperature = %.02f %c\n", temperature, TEMPERATURE_UNITS);
+
+            sprintf(PUB_PAYLOAD_SCR_T,"T = %.02f %c 0x%03x batt = %.02f %s",temperature, TEMPERATURE_UNITS,result,battery, CYW43_HOST_NAME);
+            payload_t_size = sizeof(PUB_PAYLOAD_SCR_T);
+             
+        }
+ 
+       vTaskDelay(22000);
+    }
+
+}
+
+void batt_task(__unused void *params) {
+    //bool on = false;
+    //adc_init();
+     
+    //adc_select_input(0);
+    
+    while (true) {   
+       adc_select_input(0);
+       result = adc_read();
+       battery = result * conversion_factor; 	
+       sprintf(tmp,"batt task 0x%03x -> %f V",result, battery);
+       ptrhead = head_tail_helper(ptrhead, ptrtail, ptrendofbuf, ptrtopofbuf, tmp);	
+	 
+ 
+       vTaskDelay(22000);
+    }
+
+}
+
+/*needed for ntp*/
+void ntp_task(__unused void *params) {
+    //bool on = false;
+    //printf("ntp_task starts\n");
+	run_ntp_test();
+    while (true) {
+#if 0 && configNUM_CORES > 1
+        static int last_core_id;
+        if (portGET_CORE_ID() != last_core_id) {
+            last_core_id = portGET_CORE_ID();
+            printf("ntp now from core %d\n", last_core_id);
+        }
+#endif
+        //cyw43_arch_gpio_put(0, on);
+        //on = !on;
+        
+        vTaskDelay(2200);
+    }
+}
+
+void socket_task(__unused void *params) {
+	
+	//printf("socket_task starts\n");
+    TCP_SERVER_T *state = tcp_server_init();
+    if (!state) {
+        return;
+    }
+    if (!tcp_server_open(state)) {
+        tcp_server_result(state, -1);
+        return;
+    }
+    
+
+    while (true) {
+		if(state->complete) {
+			free(state);
+			sleep_ms(1000);
+			TCP_SERVER_T *state = tcp_server_init();
+    		if (!state) {
+        		return;
+    		}
+    		if (!tcp_server_open(state)) {
+        		tcp_server_result(state, -1);
+        		return;
+         	}
+		}
+
+        vTaskDelay(200);
+		
+    }
 }
 
 void blink_task(__unused void *params) {
@@ -56,6 +249,17 @@ void blink_task(__unused void *params) {
     }
 }
 
+void preptopidata() {
+sprintf(client_message,"0123456789012345678901234567890123456789012345678901234567890123\
+0123456789012345678901234567890123456789012345678901234567890123\
+0123456789012345678901234567890123456789012345678901234567890123\
+0123456789012345678901234567890123456789012345678901234567890123\
+0123456789012345678901234567890123456789012345678901234567890123\
+012345678901234567890123456789012345678901234567890123456789012301234567890123456789012345\
+6789012345678901234567890123456789012301234567890123456789012345678901234567890123456789012345678901\n");
+ 
+}
+
 void main_task(__unused void *params) {
     if (cyw43_arch_init()) {
         printf("failed to initialise\n");
@@ -69,8 +273,10 @@ void main_task(__unused void *params) {
     } else {
         printf("Connected.\n");
     }
-
+    xTaskCreate(socket_task, "SOCKETThread", configMINIMAL_STACK_SIZE, NULL, SOCKET_TASK_PRIORITY, NULL);
     xTaskCreate(blink_task, "BlinkThread", configMINIMAL_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
+    xTaskCreate(adc_task, "ADCThread", configMINIMAL_STACK_SIZE, NULL, ADC_TASK_PRIORITY, NULL);
+    //xTaskCreate(batt_task, "BATTThread", configMINIMAL_STACK_SIZE, NULL, BATT_TASK_PRIORITY, NULL);
 
     cyw43_arch_lwip_begin();
 #if CLIENT_TEST
@@ -110,7 +316,13 @@ void vLaunch( void) {
 int main( void )
 {
     stdio_init_all();
-
+    preptopidata();
+	ptrhead = (char *)&client_message[0];
+	ptrtail = (char *)&client_message[0];
+	ptrtopofbuf = (char *)&client_message[0];
+	ptrendofbuf = (char *)&client_message[BUF_SIZE-1];
+	printf("0x%x 0x%x 0x%x 0x%x \n", ptrhead, ptrtail, ptrendofbuf, ptrtopofbuf);
+    
     /* Configure the hardware ready to run the demo. */
     const char *rtos_name;
 #if ( configNUMBER_OF_CORES > 1 )
