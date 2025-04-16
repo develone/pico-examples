4886332059d120afcce793144052b56ca47e69ff
Edward Vidal Jr <develone@sbcglobal.net>
2025-04-16 16:40:56 -0600

    tested with WIFI_SSID=ATTtpHTfPi_Guest after modifing WIFI_PASSWORD which was have with # character

--- compile-rp2040.sh
+++ compile-rp2040.sh
@@ -2,6 +2,6 @@
 rm -rf build-rp2040 
 mkdir build-rp2040
 cd build-rp2040
-cmake .. -DPICO_BOARD=pico_w -DPICO_PLATFORM=rp2040 -DWIFI_SSID="Pico_w-Net3" -DWIFI_PASSWORD="abdcda5860"
+cmake .. -DPICO_BOARD=pico_w -DPICO_PLATFORM=rp2040 -DWIFI_SSID="WIFI_SSID" -DWIFI_PASSWORD="WIFI_PASSWORD"
 make
 
--- compile-rp2350.sh
+++ compile-rp2350.sh
@@ -2,6 +2,6 @@
 rm -rf build-rp2350 
 mkdir build-rp2350
 cd build-rp2350
-cmake .. -DPICO_BOARD=pico2_w -DPICO_PLATFORM=rp2350 -DWIFI_SSID="Pico_w-Net3" -DWIFI_PASSWORD="abdcda5860"
+cmake .. -DPICO_BOARD=pico2_w -DPICO_PLATFORM=rp2350 -DWIFI_SSID="WIFI_SSID" -DWIFI_PASSWORD="WIFI_PASSWORD"
 make
 
